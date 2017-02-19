3daf16c81b7738db3ea141c149e821927d4677e5
Chenyu <chenyu19921103@gmail.com>
2017-02-18 17:48:26 -0800

    trivial model for rating prediction

---
+++ Homework3/rating prediction/baselines.py
@@ -0,0 +1,24 @@
+import gzip
+from collections import defaultdict
+
+def readGz(f):
+  for l in gzip.open(f):
+    yield eval(l)
+
+### Rating baseline: compute averages for each user, or return the global average if we've never seen the user before
+
+allRatings = []
+userRatings = defaultdict(list)
+
+for l in readGz("train.json.gz"):
+    user,item = l['reviewerID'],l['itemID']
+    allRatings.append(l['rating'])
+    userRatings[user].append(l['rating'])
+
+alpha = 1.0 * sum(allRatings[:100000]) / 100000
+
+error = 0
+for i in range(100000, 200000):
+    error = error + abs(allRatings[i] - alpha)
+print alpha
+print error / 100000
