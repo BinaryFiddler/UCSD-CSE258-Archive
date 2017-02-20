7c4f91131b0535422a37091f90f64d93797d1964
Chenyu <chenyu19921103@gmail.com>
2017-02-20 03:57:56 -0800

    deleted unneccessary code

--- Homework3/rating prediction/baselines.py
+++ Homework3/rating prediction/baselines.py
@@ -8,17 +8,16 @@ def readGz(f):
 ### Rating baseline: compute averages for each user, or return the global average if we've never seen the user before
 
 allRatings = []
-userRatings = defaultdict(list)
 
 for l in readGz("train.json.gz"):
-    user,item = l['reviewerID'],l['itemID']
     allRatings.append(l['rating'])
-    userRatings[user].append(l['rating'])
 
 alpha = 1.0 * sum(allRatings[:100000]) / 100000
 
 error = 0
+
 for i in range(100000, 200000):
-    error = error + (allRatings[i] - alpha) ** 2
+    error = error + (allRatings[i] - alpha) * (allRatings[i] - alpha)
+
 print alpha
 print error / 100000
