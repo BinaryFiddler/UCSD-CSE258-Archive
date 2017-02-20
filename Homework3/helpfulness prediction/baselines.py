53e702ce9bc4b785c2ac143e5b0a3423d83e99af
Chenyu <chenyu19921103@gmail.com>
2017-02-19 16:06:21 -0800

    seemly correct implementation for this model

--- Homework3/helpfulness prediction/baselines.py
+++ Homework3/helpfulness prediction/baselines.py
@@ -12,55 +12,45 @@ print "Reading data..."
 data = list(readGz("train.json.gz"))
 print "done"
 
-#
-# allRatings = []
-# allLength = []
-# nhelpful = []
-# outof = []
-# userRatings = defaultdict(list)
-#
-# for l in readGz("train.json.gz"):
-#       user,item = l['reviewerID'],l['itemID']
-#       allRatings.append(l['rating'])
-#       allLength.append(len(l['reviewText']))
-#       nhelpful.append(l['helpful']['nHelpful'])
-#       outof.append(l['helpful']['outOf'])
-#       userRatings[user].append(l['rating'])
+train = data[:100000]
+test = data[100000:]
+x = []
+y = []
+for l in train:
+    helpInfo, rate, num =l['helpful'],l['rating'], len(l['reviewText'].split(" "))
+    out = helpInfo['outOf']
+    nhelp = helpInfo['nHelpful']
+    if out > 0:
+        y.append(nhelp * 1.0 / out)
+        ls = [1, num*1.0, rate*1.0]
+        x.append(ls)
 
-def feature(datum):
-  feat = [1]
-  feat.append(len(datum['reviewText']))
-  feat.append(datum['rating'])
-  return feat
+x = np.matrix(x)
+y = np.matrix(y)
+theta = np.linalg.inv(x.T * x) * x.T * y.T
 
-# building the training set
-Xtrain = [feature(d) for d in data[:100000] if d['helpful']['outOf'] != 0]
-ytrain = [d['helpful']['nHelpful'] / d['helpful']['outOf'] for d in data[:100000] if d['helpful']['outOf'] != 0]
+print theta
 
-print len(Xtrain), len(ytrain)
+x = []
+yhelp = []
+yout = []
+for l in test:
+    helpInfo, rate, num =l['helpful'],l['rating'], len(l['reviewText'].split(" "))
+    out = helpInfo['outOf']
+    nhelp = helpInfo['nHelpful']
+    yhelp.append(nhelp * 1.0)
+    yout.append(out * 1.0)
+    ls = [1, num*1.0, rate*1.0]
+    x.append(ls)
 
-Xtrain = np.matrix(Xtrain)
-ytrain = np.matrix(ytrain)
-thetas = np.linalg.inv(Xtrain.T * Xtrain) * Xtrain.T * ytrain.T
-
-# building the validation set
-Xvalid = [feature(d) for d in data[100000:]]
-yvalid = np.array([[d['helpful']['nHelpful'] for d in data[100000:]]])
-
-predicted = np.matrix(Xvalid) * thetas
-predicted = predicted.T
-predicted = np.array(predicted)
-yvalidOutOf = np.array([[d['helpful']['outOf'] for d in data[100000:]]])
+predicted = np.matrix(x) * theta
+predicted = np.array(predicted.T)
+yout = np.array(yout)
+realPrediction = np.multiply(predicted, yout)
 
 def mae(a, b):
 	error = np.sum(np.absolute(np.subtract(a, b)))
 	return error
 
+print mae(realPrediction, np.array(yhelp)) / 100000
 
-print yvalidOutOf
-print yvalidOutOf.shape, type(yvalidOutOf)
-print predicted.shape, type(predicted)
-
-nHelpPrediction = np.multiply(predicted,yvalidOutOf)
-print nHelpPrediction
-print mae(nHelpPrediction, yvalid) / 100000
